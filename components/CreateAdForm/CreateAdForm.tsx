6e694a1062df2129bb4a4eb44cfee9427724d4b2
BearLikeCode <bearlikecode@gmail.com>
2020-09-25 11:47:45 +0300

    Fix landing and add validationSchema to Formik

--- components/CreateAdForm/CreateAdForm.tsx
+++ components/CreateAdForm/CreateAdForm.tsx
@@ -1,99 +1,152 @@
-import {Formik, Form,Field} from "formik"
+import { Formik, Form, Field } from 'formik'
 import fire from '../../config/fire'
 import styles from './createAdForm.module.scss'
 import * as yup from 'yup'
-import { useState } from "react"
+import { useState } from 'react'
 
-const yupSchema = yup({
+const validationSchema = yup.object({
     nameAd: yup.string().required('Please enter Advertisement name'),
-    descriptionAd: yup.string().required('Please enter Advertisement description'),
+    descriptionAd: yup
+        .string()
+        .required('Please enter Advertisement description'),
     placeAd: yup.array().required('Please '),
-    statusAd: yup.string().required('Please enter Advertisement status')
+    statusAd: yup.string().required('Please enter Advertisement status'),
 })
 
 function CreateAdForm() {
-    const [defPlace, setDefPlace] = useState(['Google'])
+    const [defPlaceAd, setDefPlaceAd] = useState(['Google'])
+    const [defStatusAd, setDefStatusAd] = useState('Public')
 
     return (
-        <>
+        <div className="container">
             <Formik
+                validationSchema={validationSchema}
                 initialValues={{
                     nameAd: '',
                     descriptionAd: '',
                     placeAd: '',
-                    statusAd: ''
+                    statusAd: '',
                 }}
                 onSubmit={(values) => {
-                    event.preventDefault();
+                    event.preventDefault()
                     fire.firestore()
                         .collection('ads')
-                        .add({...values});
+                        .add({ ...values })
                     alert('Success')
-
                 }}
             >
                 {({ errors, touched }) => (
                     <Form className={styles.form}>
-                        <label htmlFor="nameAd" className={styles.label}>Advertisment name:
-                            <Field id="nameAd" name="nameAd" placeholder="Advertisment name" className={styles.field}/>
+                        <label htmlFor="nameAd" className={styles.label}>
+                            Advertisment name:
+                            <Field
+                                id="nameAd"
+                                name="nameAd"
+                                placeholder="Advertisment name"
+                                className={styles.field}
+                            />
                             {errors.nameAd && touched.nameAd ? (
-                                <div>
-                                    {errors.nameAd}
-                                </div>
+                                <div>{errors.nameAd}</div>
                             ) : null}
                         </label>
-                        <label htmlFor="descriptionAd" className={styles.label}>Advertisment description:
-                            <Field id="descriptionAd" name="descriptionAd" placeholder="Advertisment description" className={styles.field}/>
+                        <label htmlFor="descriptionAd" className={styles.label}>
+                            Advertisment description:
+                            <Field
+                                id="descriptionAd"
+                                name="descriptionAd"
+                                placeholder="Advertisment description"
+                                className={styles.field}
+                            />
                             {errors.descriptionAd && touched.descriptionAd ? (
-                                <div>
-                                    {errors.descriptionAd}
-                                </div>
-                            ):null}
+                                <div>{errors.descriptionAd}</div>
+                            ) : null}
                         </label>
                         <label>
-                            <div id="checkbox-group">Select place of advertisment:</div>
+                            <div id="checkbox-group">
+                                Select place of advertisment:
+                            </div>
                             <div role="group" aria-labelledby="checkbox-group">
-                                <label className={styles.labelCheckbox}>Google
-                                <Field type="checkbox" name="placeAd" value="Google" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    Google
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="Google"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
-                                <label className={styles.labelCheckbox}>Facebook
-                                <Field type="checkbox" name="placeAd" value="Facebook" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    Facebook
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="Facebook"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
-                                <label className={styles.labelCheckbox}>Instagram
-                                <Field type="checkbox" name="placeAd" value="Instagram" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    Instagram
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="Instagram"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
-                                <label className={styles.labelCheckbox}>Youtube
-                                <Field type="checkbox" name="placeAd" value="Youtube" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    Youtube
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="Youtube"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
-                                <label className={styles.labelCheckbox}>Baners
-                                <Field type="checkbox" name="placeAd" value="Baners" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    Baners
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="Baners"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
-                                <label className={styles.labelCheckbox}>TV
-                                <Field type="checkbox" name="placeAd" value="TV" className={styles.fieldCheckbox} />
+                                <label className={styles.labelCheckbox}>
+                                    TV
+                                    <Field
+                                        type="checkbox"
+                                        name="placeAd"
+                                        value="TV"
+                                        className={styles.fieldCheckbox}
+                                    />
                                 </label>
                             </div>
                         </label>
                         <div id="my-radio-group">Select status:</div>
                         <div role="group" aria-labelledby="my-radio-group">
                             <label>
-                            <Field type="radio" name="statusAd" value="Public" />
-                            Public
+                                <Field
+                                    type="radio"
+                                    name="statusAd"
+                                    value="Public"
+                                />
+                                Public
                             </label>
                             <label>
-                            <Field type="radio" name="statusAd" value="Draft" />
-                            Draft
+                                <Field
+                                    type="radio"
+                                    name="statusAd"
+                                    value="Draft"
+                                />
+                                Draft
                             </label>
                         </div>
-                        <button
-                            type="submit"
-                        >
-                            Submit
-                        </button>
+                        <button type="submit">Submit</button>
                     </Form>
                 )}
             </Formik>
-        </>
+        </div>
     )
 }
 
-export default CreateAdForm;
+export default CreateAdForm
