8b4609a5feea42902d9f1aa3ed778fa2ba4cfb4a
ataman-v-massmediagroup <mmg.ataman.v@gmail.com>
2020-10-05 09:57:07 +0300

    Updated ProfileDetails.tsx Added method logout

--- components/ProfileDetails/ProfileDetails.tsx
+++ components/ProfileDetails/ProfileDetails.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/Link'
 import { CreatedAds } from '..'
+import {useContext, useState} from "react";
+import {AuthContext} from "../../context/AuthContext";
 
 function ProfileDetails() {
+    const { logoutUser } = useContext(AuthContext)
+
+    const handleLogout = () => {
+        logoutUser()
+    }
+
     return (
         <div className="container">
             <p>Profile</p>
+            <button onClick={handleLogout}>Logout</button>
             <Link href="/ad/createAd">Create ad</Link>
 
             <CreatedAds />
