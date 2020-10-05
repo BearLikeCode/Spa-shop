154893948038a5af67aecb7d3777caa483ea29c0
ataman-v-massmediagroup <mmg.ataman.v@gmail.com>
2020-10-05 10:40:23 +0300

    Updated Navbar component, move check for auth user

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,32 @@
 import Link from 'next/Link'
 import styles from './navbar.module.scss'
 import classNames from 'classnames'
+import {useContext} from "react";
+import {AuthContext} from "../../context/AuthContext";
 
 function Navbar() {
+    const { auth } = useContext(AuthContext)
+
     return (
         <div className={styles.navbar}>
             <div className={classNames('container', styles.navbarRow)}>
                 <Link href="/">
                     <p className={styles.navbarLogo}>SPA Shop</p>
                 </Link>
-                <Link href="/user/profile">User</Link>
+                <div>
+                    {!auth ? (
+                        <>
+                            <Link href="/user/register">
+                                <a>Register</a>
+                            </Link>{' '}
+                            |
+                            <Link href="/user/login">
+                                <a> Login</a>
+                            </Link>
+
+                        </>
+                    ) :<Link href="/user/profile">User</Link>}
+                </div>
             </div>
         </div>
     )
