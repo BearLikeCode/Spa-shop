014c51701e096c45287a839936989cffe6ada6c1
ataman-v-massmediagroup <mmg.ataman.v@gmail.com>
2020-10-21 16:46:41 +0300

    Add progressAds component

--- components/index.tsx
+++ components/index.tsx
@@ -4,9 +4,10 @@ import CreatedAds from './createdAds/createdAds'
 import ListAds from './listAds/listAds'
 import AdBlockIndexPage from './adBlockIndexPage/adBlockIndexPage'
 import ProfileDetails from './profileDetails/profileDetails'
-import AdDetails from './adDetails/adDetails';
-import LoginForm from './loginForm/loginForm';
-import SignupForm from './signupForm/signupForm';
+import AdDetails from './adDetails/adDetails'
+import LoginForm from './loginForm/loginForm'
+import SignupForm from './signupForm/signupForm'
+import ProgressAds from './progressAds/progressAds'
 
 export {
     Navbar,
@@ -18,4 +19,5 @@ export {
     AdDetails,
     LoginForm,
     SignupForm,
+    ProgressAds,
 }
